95b1c7e3717c046d5656ff0ad4b8d115bd523322
Matthew-Jordon Whynter <mattwhynt@yahoo.com>
2020-11-01 17:59:55 -0500

    added js code

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-/* Add your JavaScript to this file */
+window.onload = function() {
+    var btn = document.querySelector("button");
+    var msg = document.getElementsByClassName("message");
+    btn.addEventListener("click", function(event) {
+        event.preventDefault();
+        var text = document.getElementById("email").value;
+        if (text != "") {
+            msg[0].innerHTML = "Thank you! Your email addres " + text + " has been added to our mailing list!";
+        } else {
+            msg[0].innerHTML = "Please enter a valid email address";
+        }
+    });
+}
